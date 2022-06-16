c7269623cc6ca52ae2ccca901e6ea7188736ae2b
Stanislav Semenenko <stanichgame@gmail.com>
2022-06-17 00:37:13 +0300

    user creation serializer bug fixed

--- pizzalab/apps/user/serializers.py
+++ pizzalab/apps/user/serializers.py
@@ -123,4 +123,5 @@ class UserCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(email=validated_data.get('email'))
         user.set_password(validated_data.get('password'))
+        user.save(update_fields=('password',))
         return user
