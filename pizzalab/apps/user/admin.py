139b0167f189eafa6fb2ed735bb101c5c75489eb
Stanislav Semenenko <stanichgame@gmail.com>
2022-06-17 00:34:21 +0300

    improved user admin

--- pizzalab/apps/user/admin.py
+++ pizzalab/apps/user/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 from user.models import Profile
 from user.models import Address
 
 
 @admin.register(get_user_model())
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UsersAdmin(UserAdmin):
+    ordering = ('email', 'last_login')
+    list_filter = ('created_at', 'updated_at', 'last_login')
+    list_display = ('email', 'last_login')
+    fieldsets = (
+        ('Personal info', {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('created_at', 'updated_at', 'last_login')})
+    )
+    readonly_fields = ('created_at', 'updated_at')
+    exclude = ('first_name', 'last_name', 'username', 'is_active')
 
 
 @admin.register(Profile)
